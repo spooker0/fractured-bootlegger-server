ff7b893af438003bbf2e2bbe189508b8f99617dc
rukqoa <rukqoa@gmail.com>
2019-08-13 09:49:54 -0700

    Remove email auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
         }
     });
 
-    let discordScopes = ['identify', 'email'];
+    let discordScopes = ['identify'];
     let discordStrategy = new DiscordStrategy({
         clientID: config.discordId,
         clientSecret: config.discordSecret,
